2f3dc4ea1c823ac0050c712d51d0e36fce89a65a
antailbaxt3r <arjunbajpaiwork@gmail.com>
2020-03-24 20:12:38 +0530

    Completed SignupActivity

--- app/src/main/java/com/antailbaxt3r/petmanagerapp/SignupActivity.java
+++ app/src/main/java/com/antailbaxt3r/petmanagerapp/SignupActivity.java
@@ -1,14 +1,25 @@
 package com.antailbaxt3r.petmanagerapp;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
 
   EditText emailE, passE, nameE;
   Button signupB, loginB;
+  FirebaseAuth auth;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +27,32 @@ public class SignupActivity extends AppCompatActivity {
     setContentView(R.layout.activity_signup);
     attachID();
 
+    loginB.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Intent login = new Intent(SignupActivity.this, LoginActivity.class);
+        startActivity(login);
+        finish();
+      }
+    });
+
+    signupB.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        auth.createUserWithEmailAndPassword(emailE.getText().toString(), passE.getText().toString())
+            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+              @Override public void onSuccess(AuthResult authResult) {
+                Toast.makeText(SignupActivity.this, "User created in!", Toast.LENGTH_SHORT).show();
+                Intent main = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(main);
+                finish();
+              }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+              @Override public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignupActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+              }
+            });
+      }
+    });
 
   }
 
@@ -25,5 +62,6 @@ public class SignupActivity extends AppCompatActivity {
     nameE = findViewById(R.id.name);
     signupB = findViewById(R.id.sign_up);
     loginB = findViewById(R.id.log_in);
+    auth = FirebaseAuth.getInstance();
   }
 }
